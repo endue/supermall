6cb88a1416bb54d4a45db22e784fd15b838bb531
limeng17 <809653838@qq.com>
2021-02-20 08:05:56 +0800

    详情页基础文字组件功能实现

--- src/network/detail.js
+++ src/network/detail.js
@@ -8,3 +8,17 @@ export function getDetail(iid) {
     }
   })
 }
+
+// 创建类Goods，在初始化类时调用构造方法constructor
+// const g = new Goods(itemInfo,columns,services)
+export class Goods {
+  constructor(itemInfo, columns, services){
+    this.title = itemInfo.title
+    this.desc = itemInfo.desc
+    this.newPrice = itemInfo.price
+    this.oldPrice = itemInfo.oldPrice
+    this.realPrice = itemInfo.lowNowPrice
+    this.discount = columns
+    this.services = services
+  }
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,34 +2,42 @@
   <div id="detail">
     <detail-nav-bar/>
     <detail-swiper :top-images="topImages"/>
+    <detail-base-info :goods="goods"/>
   </div>
 </template>
 
 <script>
   import DetailNavBar from './childComps/DetailNavBar'
   import DetailSwiper from './childComps/DetailSwiper'
+  import DetailBaseInfo from './childComps/DetailBaseInfo'
 
-  import {getDetail} from 'network/detail'
+  import {getDetail, Goods} from 'network/detail'
 
   export default {
     name: "Detail",
     components: {
       DetailSwiper,
-      DetailNavBar
+      DetailNavBar,
+      DetailBaseInfo
     },
     data() {
       return {
         iid: null,
-        topImages: []
+        topImages: [],
+        goods: null
       }
     },
     created() {
       this.iid = this.$route.params.iid
       // 请求详情数据
       getDetail(this.iid).then(res => {
+        const data = res.result
         // 获取顶部轮播图
-        this.topImages = res.result.itemInfo.topImages
+        this.topImages = data.itemInfo.topImages
+        // 获取商品信息,这里初始化一个Goods对象，将数据整合同步给子组件
+        this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
       })
+
     }
   }
 </script>
