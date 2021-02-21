94d77877438852c8c7f836ce687f2565db5a3707
limeng17 <809653838@qq.com>
2021-02-21 11:08:11 +0800

    初始化购物车列表CartList以及列表商品CartListItem组件

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,15 +1,39 @@
 <template>
-    <div>
-      购物车
+    <div class="cart">
+      <nav-bar class="nav-bar">
+        <div slot="center">购物车({{cartLength}})</div>
+      </nav-bar>
+
+      <cart-list></cart-list>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "Cart"
+  import NavBar from 'components/common/navbar/NavBar'
+  import CartList from "./childComps/CartList";
+  import {mapGetters} from 'vuex'
+
+  export default {
+    name: "Cart",
+    components: {
+      CartList,
+      NavBar
+    },
+    computed: {
+      ...mapGetters([
+        'cartLength'
+      ])
     }
+  }
 </script>
 
 <style scoped>
-
+  .cart {
+    height: 100vh;
+  }
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #F0F0F0;
+    font-weight: 700;
+  }
 </style>
